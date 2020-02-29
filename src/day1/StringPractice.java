f62e0c0f2572b5dd0477fa67e75b065b8fb0bb06
mohammedhusein <ayamilo2000@gmail.com>
2020-02-29 15:25:31 -0500

    modified

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -1,7 +1,13 @@
 package day1;
 
 public class StringPractice {
+    void str(float d,int k){
+        System.out.println(d);
+    }void str(int o,float w){}
+
     public static void main(String[] args) {
         System.out.println("Hello, world");
+
+
     }
 }
